aeebef68503876191fc48cb109a3c7efecdcd9d4
Yongzhuang Liu lab <yzhuangliu@gmail.com>
2025-05-29 14:30:46 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name = 'scpilot',
     version = '0.1',
     author = 'Jialiang Wang',
-    author_email = '25B303016@stu.hit.edu.cn',
+    author_email = '18846091447@163.com',
+    license = 'MIT',
     packages = find_packages(include=['scpilot', 'scpilot.*']),
     zip_safe = False,
 )
